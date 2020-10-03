4c4013b3810f98002df6044a4c65b17399a6696c
ExpLuuk <luukdomhof@exp-plus.nl>
2020-10-03 14:14:05 +0200

    add basic player controls and camera movement

---
+++ Assets/Modules/Player/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ExpPlus.LD47.Player {
+
+    public class PlayerController : MonoBehaviour {
+
+        private Vector2 movementInput;
+        private Vector2 orientationalInput;
+
+        private Rigidbody2D rigidbody;
+        public Camera camera;
+
+        public float speed;
+
+
+        // Start is called before the first frame update
+        void Start() {
+
+            rigidbody = GetComponent<Rigidbody2D>();
+            //camera = Camera.main;
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+            Vector3 pointerWorldPos = camera.ScreenToWorldPoint(new Vector3(orientationalInput.x, orientationalInput.y, camera.nearClipPlane));
+            Vector3 direction = pointerWorldPos - transform.position;
+            Debug.DrawLine(transform.position, pointerWorldPos, Color.green);
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        }
+
+        private void FixedUpdate() {
+
+            rigidbody.velocity = movementInput * speed;
+        }
+
+        public void GetMovementInput(InputAction.CallbackContext context) {
+
+            movementInput = context.ReadValue<Vector2>();
+        }
+
+        public void GetOrientationalInput(InputAction.CallbackContext context) {
+
+            orientationalInput = context.ReadValue<Vector2>();
+        }
+    }
+}
