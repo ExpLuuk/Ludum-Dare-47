1e9f998a47e6e0f8e8bacd7ac3dd625bf9897c77
ExpLuuk <luukdomhof@exp-plus.nl>
2020-10-03 22:18:08 +0200

    add altars

---
+++ Assets/Modules/Altar/Scripts/Altar.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using ExpPlus.LD47.Common;
+using ExpPlus.LD47.Weapons;
+
+namespace ExpPlus.LD47.Altar {
+
+    public class Altar : MonoBehaviour {
+
+        [Header("References")]
+        public GameObject player;
+
+        [Header("Config")]
+        public Element element;
+        public float interactionRadius = 5f;
+
+        private void Start() {
+
+            if (!player)
+                player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        private void AttemptToUseAltar() {
+
+            if(Vector3.Distance(transform.position, player.transform.position) <= interactionRadius) {
+
+                player.GetComponent<WeaponController>().SwitchElement(element);
+            }
+        }
+
+        public void GetAltarInteractionInput(InputAction.CallbackContext context) {
+
+            if (context.performed) {
+
+                Debug.Log("Activated");
+
+                AttemptToUseAltar();
+            }
+        }
+    }
+}
